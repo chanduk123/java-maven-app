557b4a932dea1aa62ea5196a428acd470e48b297
kanuri chandu <34050981+chanduk123@users.noreply.github.com>
2022-01-20 19:46:32 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -6,9 +6,9 @@ def buildJar() {
 def pushImage() {
     echo 'building docker image and pushing it private repo'
     withCredentials([usernamePassword(credentialsId:'docker-cred', usernameVariable:'USER', passwordVariable:'pass')])
-    sh 'docker build -t chandukanuri/demo-app:jma-lts'
-    sh "Echo $pass | docker login -u $USER"
-    sh 'docker pus chandukanuri/demo-app:jma-lts'
+    sh 'docker build -t chandukanuri/demo-app:jma-lts .'
+    sh "echo $pass | docker login -u $USER --password-stdin"
+    sh 'docker push chandukanuri/demo-app:jma-lts'
 }
 
 def deploy() {
